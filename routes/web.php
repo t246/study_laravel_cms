cdd080ed6b86390f94000eca266b6acb51d5a0db
t246 <takashinishimura@Takashi-no-MacBook-Air.local>
2017-08-12 07:30:14 +0900

    Bootstrapのリンク追加、validation追加

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,21 @@ Route::post('/books', function(Request $request) {
 Route::delete('/book/{book}', function(Book $book) {
     //
 });
+
+Route::post('/book', function(Request $request) {
+    $validator = Validator::make($request->all(), [
+        'item_name' => 'require|max:255',
+    ]);
+
+    if($validation->fails()) {
+        return redirect('/')->withInput()->withErrors($validator);
+    }
+
+    $books = new Book;
+    $book->item_name = $request->item_name;
+    $book->item_number = '1';
+    $book->item_amount = '1000';
+    $book->published = '2017-03-07 00:00:00';
+    $book->save();
+    return redirect('/');
+});
