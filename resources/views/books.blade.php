6c7cc3a3e071f5c2be550fe65fe77e6f17c6c89e
t246 <takashinishimura@Takashi-no-MacBook-Air.local>
2017-08-11 19:54:36 +0900

    テンプレート追加（books,app）

---
+++ resources/views/books.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="panel-body">
+        @include('common.errors')
+        <form action="{{url('books')}}" method="POST" class="form-horizontal">
+            <div class="form-group">
+                <label for="book" class="col-sm-3 control-label">
+                    Book
+                </label>
+                <div class="col-sm-6">
+                    <input type="text" name="item_name" id="book-name" class="form-control">
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="col-sm-offset-3 col-sm-6">
+                    <button type="submit" class="btn btn-default">
+                        <i class="glyphicon glyphicon-plus"></i>Save
+                    </button>
+            </div>
+        </form>
+    </div>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Book List</title>
+    </head>
+
+    <body>
+        <div class="container">
+            <nav class="navbar navbar-default">
+
+            </nav>
+        </div>
+
+        @yield('content')
+    </body>
+</html>
