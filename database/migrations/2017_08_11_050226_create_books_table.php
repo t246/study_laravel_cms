c459249bb3dfd0e5f77d46c326b50f1a7b832d34
t246 <takashinishimura@Takashi-no-MacBook-Air.local>
2017-08-11 18:58:19 +0900

    migration追記

--- database/migrations/2017_08_11_050226_create_books_table.php
+++ database/migrations/2017_08_11_050226_create_books_table.php
@@ -15,8 +15,25 @@ class CreateBooksTable extends Migration
     {
         Schema::create('books', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('item_name');
+            $table->integer('item_number');
+            $table->integer('item_amount');
+            $table->dateTime('published');
             $table->timestamps();
         });
+
+        /* うまくいかないので
+        create table books (
+            id MEDIUMINT NOT NULL AUTO_INCREMENT,
+            item_name varchar(191) not null,
+            item_number mediumint not null,
+            item_amount mediumint not null,
+            published datetime not null,
+            created_at timestamp ,
+            updated_at timestamp default current_timestamp,
+            primary key(id)
+        );
+        */
     }
 
     /**
