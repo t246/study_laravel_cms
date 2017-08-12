6e6b8dcbf793ca50c7c9842eed27747675f84713
t246 <takashinishimura@Takashi-no-MacBook-Air.local>
2017-08-12 11:32:15 +0900

    更新画面追加、項目追加、Controllerのモック作成

--- routes/web.php
+++ routes/web.php
@@ -32,9 +32,9 @@ Route::post('/books', function(Request $request) {
 
     $books = new Book;
     $books->item_name = $request->item_name;
-    $books->item_number = '1';
-    $books->item_amount = '1000';
-    $books->published = '2017-03-07 00:00:00';
+    $books->item_number = $request->item_number;
+    $books->item_amount = $request->item_amount;
+    $books->published = $request->published;
     $books->save();
     return redirect('/');
     //
@@ -44,4 +44,32 @@ Route::delete('/book/{book}', function(Book $book) {
     $book->delete();
     return redirect('/');
     //
+});
+
+Route::post('/books/update', function(Request $request) {
+    $validator = Validator::make($request->all(), [
+        'id' => 'required',
+        'item_name' => 'required|min:3|max:255',
+        'item_number' => 'required|min:1|max:3',
+        'item_amount' => 'required|max:6',
+        'published' => 'required',
+    ]);
+
+    if($validator->fails()) {
+        return redirect('/')->withInput()->withErrors($validator);
+    }
+
+    $books = Book::find($request->id);
+    $books->item_name = $request->item_name;
+    $books->item_number = $request->item_number;
+    $books->item_amount = $request->item_amount;
+    $books->published = $request->published;
+    $books->save();
+
+    return redirect('/');
+});
+
+Route::post('/booksedit/{books}', function(Book $books) {
+    //{books}id 値を取得 => Book $books id 値の1レコード取得
+    return view('booksedit', ['book' => $books]);
 });
