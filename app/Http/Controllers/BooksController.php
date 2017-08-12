7e67c7dbc16d6b4759dc3614efb333a1d0058ede
t246 <takashinishimura@Takashi-no-MacBook-Air.local>
2017-08-12 12:01:09 +0900

    リファクタ（ロジックをControllerに移動）

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -9,9 +9,67 @@ use Validator;
 
 class BooksController extends Controller
 {
+    public function index() {
+        $books = Book::orderBy('created_at', 'asc')->get();
+        return view('books', [
+            'books' => $books
+        ]);
+    }
+
+    public function edit(Book $books)
+    {
+        //{books}id 値を取得 => Book $books id 値の1レコード取得
+        return view('booksedit', [
+            'book' => $books,
+        ]);
+    }
+
     public function update(Request $request)
     {
+        $validator = Validator::make($request->all(), [
+            'id' => 'required',
+            'item_name' => 'required|min:3|max:255',
+            'item_number' => 'required|min:1|max:3',
+            'item_amount' => 'required|max:6',
+            'published' => 'required',
+        ]);
 
+        if($validator->fails()) {
+            return redirect('/')->withInput()->withErrors($validator);
+        }
+
+        $books = Book::find($request->id);
+        $books->item_name = $request->item_name;
+        $books->item_number = $request->item_number;
+        $books->item_amount = $request->item_amount;
+        $books->published = $request->published;
+        $books->save();
+
+        return redirect('/');
     }
     //
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'item_name' => 'required|max:255',
+        ]);
+
+        if($validator->fails()) {
+            return redirect('/')->withInput()->withErrors($validator);
+        }
+
+        $books = new Book;
+        $books->item_name = $request->item_name;
+        $books->item_number = $request->item_number;
+        $books->item_amount = $request->item_amount;
+        $books->published = $request->published;
+        $books->save();
+        return redirect('/');
+    }
+
+    public function destroy(Book $book)
+    {
+        $book->delete();
+        return redirect('/');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -13,63 +13,12 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::get('/', function () {
-    $books = Book::orderBy('created_at', 'asc')->get();
-    return view('books', [
-        'books' => $books
-    ]);
-    //return view('welcome');
-});
+Route::get('/', 'BooksController@index');
 
-Route::post('/books', function(Request $request) {
-    $validator = Validator::make($request->all(), [
-        'item_name' => 'required|max:255',
-    ]);
+Route::post('/books', 'BooksController@store');
 
-    if($validator->fails()) {
-        return redirect('/')->withInput()->withErrors($validator);
-    }
+Route::delete('/book/{book}', 'BooksController@destroy');
 
-    $books = new Book;
-    $books->item_name = $request->item_name;
-    $books->item_number = $request->item_number;
-    $books->item_amount = $request->item_amount;
-    $books->published = $request->published;
-    $books->save();
-    return redirect('/');
-    //
-});
+Route::post('/books/update', 'BooksController@update');
 
-Route::delete('/book/{book}', function(Book $book) {
-    $book->delete();
-    return redirect('/');
-    //
-});
-
-Route::post('/books/update', function(Request $request) {
-    $validator = Validator::make($request->all(), [
-        'id' => 'required',
-        'item_name' => 'required|min:3|max:255',
-        'item_number' => 'required|min:1|max:3',
-        'item_amount' => 'required|max:6',
-        'published' => 'required',
-    ]);
-
-    if($validator->fails()) {
-        return redirect('/')->withInput()->withErrors($validator);
-    }
-
-    $books = Book::find($request->id);
-    $books->item_name = $request->item_name;
-    $books->item_number = $request->item_number;
-    $books->item_amount = $request->item_amount;
-    $books->published = $request->published;
-    $books->save();
-
-    return redirect('/');
-});
-
-Route::post('/booksedit/{books}', function(Book $books) {
-    //{books}id 値を取得 => Book $books id 値の1レコード取得
-    return view('booksedit', ['book' => $books]);
-});
+Route::post('/booksedit/{books}', 'BooksController@edit');
